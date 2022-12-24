7dfc8064960097dcb13bcb097535bfb188c90905
Dev <devendersingh2k@gmail.com>
2022-12-24 12:51:57 +0530

    code completed

--- EmailValidator.py
+++ EmailValidator.py
@@ -1,5 +1,5 @@
 email = input("Enter Email ID: ")
-
+space = upperAlpha = error = 0
 # email length should be atleast of 6 character
 if len(email) >= 6:
 
@@ -10,17 +10,28 @@ if len(email) >= 6:
         if "@" in email and email.count("@") == 1:
 
             # '.' should be placed at correct postion
-            if email[-4] == "." ^ email[-3] == ".":
+            if (email[-4] == ".") ^ (email[-3] == "."):
 
-                #gmail does not contain any spaces in it
-                if " " not in email:
-                    pass
-
+                #gmail must not contain any spaces,capital letters, special characters except '@','.','_'
+                for i in email:
+                    if i.isspace():
+                        space = 1
+                    elif i.isdigit():
+                        continue
+                    elif i.isalpha():
+                        if i == i.upper():
+                            upperAlpha = 1
+                    elif i == "_" or i == "@" or i == ".":
+                        continue
+                    else:
+                        error = 1
+                if error == 1 or upperAlpha == 1 or space == 1:
+                    print("Wrong email: Please follow the rules to enter a correct email address!")
                 else:
-                    print("Wrong mail: There should not be any spaces in email address")
+                    print("Correct Email Address!")
 
             else:
-                print("Wrong mail: '.' should be placed at correct position")
+                print("Wrong email: '.' should be placed at correct position")
         else:
             print("Wrong email: email must contain @ and it should be used once only")
 
